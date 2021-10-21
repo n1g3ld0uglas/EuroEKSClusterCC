b5d8590a767dbe509d0e96356567e1046925b4b5
n1g3ld0uglas <82048393+n1g3ld0uglas@users.noreply.github.com>
2021-10-21 09:51:30 +0100

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,20 +1,8 @@
-# Some helper functions defined first
-
-
 function remove_unwanted_policies(){
    kubectl patch felixconfiguration.p default -p '{"spec":{"flowLogsFlushInterval":"10s"}}'
    kubectl patch felixconfiguration.p default -p '{"spec":{"flowLogsFileAggregationKindForAllowed":1}}'
    kubectl patch felixconfiguration.p default -p '{"spec":{"flowLogsCollectTcpStats":true}}'
 }
-
-function remove_storefront_application() {
-    kubectl delete -f https://installer.calicocloud.io/storefront-demo.yaml
-    sleep 2
-    #echo "kubectl get pods -n storefront -o yaml"
-}
-
-# Remove zone based architecture policies
-remove_unwanted_policies
 
 kubectl delete -f https://raw.githubusercontent.com/tigera-solutions/aws-howdy-parter-calico-cloud/main/policies/dmz.yaml
 sleep 2
@@ -22,33 +10,31 @@ kubectl delete -f https://raw.githubusercontent.com/tigera-solutions/aws-howdy-p
 sleep 2
 kubectl delete -f https://raw.githubusercontent.com/tigera-solutions/aws-howdy-parter-calico-cloud/main/policies/restricted.yaml
 sleep 2
+kubectl delete -f https://installer.calicocloud.io/storefront-demo.yaml
+sleep 2
 
 
 # Delete the compliance reports
-kubectl delete -f https://raw.githubusercontent.com/tigera-solutions/aws-howdy-parter-calico-cloud/main/reporting/daily-cis-report.yaml --validate=false
+kubectl delete -f https://raw.githubusercontent.com/tigera-solutions/aws-howdy-parter-calico-cloud/main/reporting/daily-cis-report.yaml
 sleep 2
 kubectl delete -f https://raw.githubusercontent.com/tigera-solutions/aws-howdy-parter-calico-cloud/main/reporting/half-hour-inventory-report.yaml
- --validate=false
 sleep 2
-kubectl delete -f https://raw.githubusercontent.com/tigera-solutions/aws-howdy-parter-calico-cloud/main/reporting/half-hour-network-access.yaml  
- --validate=false
+kubectl delete -f https://raw.githubusercontent.com/tigera-solutions/aws-howdy-parter-calico-cloud/main/reporting/half-hour-network-access.yaml
 sleep 2
 
 
 # Delete the Rogue application
 kubectl delete -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/master/release/kubernetes-manifests.yaml
 sleep 2
-kubectl get pods -n default
 
 # Remove the Honeypod configs
 kubectl delete -f kubectl apply -f https://docs.tigera.io/manifests/threatdef/honeypod/common.yaml
+sleep 2
 kubectl delete -f https://docs.tigera.io/manifests/threatdef/honeypod/vuln-svc.yaml 
 sleep 2
-kubectl get pods -n tigera-internal -o wide
 
 # Remove GlobalThreatFeeds for Anonymization attacks
-#kubectl delete -f https://docs.tigera.io/manifests/threatdef/ejr-vpn.yaml
+kubectl delete -f https://docs.tigera.io/manifests/threatdef/ejr-vpn.yaml
+sleep 2
 kubectl delete -f https://docs.tigera.io/manifests/threatdef/tor-exit-feed.yaml
-
-# Confirm all pods have been removed
-kubectl get pods -A -w
+sleep 2
